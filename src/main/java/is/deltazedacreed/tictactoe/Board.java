96b06a13951cec57b53dfb8aaff42962626f58e1
Ingibjorg Osk Jonsdottir <ingibjorgj12@i-bb14f38e.greenqloud.internal>
2014-10-29 14:17:58 +0000

    Adapted the classes to the correct code style.

--- src/main/java/is/deltazedacreed/tictactoe/Board.java
+++ src/main/java/is/deltazedacreed/tictactoe/Board.java
@@ -1,9 +1,9 @@
- /*
-  * Copyright 2014 Delta Zeda Creed.
-  *
-*/
+// Copyright (c) 2014 Delta Zeda Creed.
 
 package is.deltazedacreed.tictactoe;
 
+/**
+ * Class constructor specifying the board of the game, TicTacToe.
+ */
 public class Board{
 }
--- src/main/java/is/deltazedacreed/tictactoe/Game.java
+++ src/main/java/is/deltazedacreed/tictactoe/Game.java
@@ -1,11 +1,11 @@
- /*
-  * Copyright 2014 Delta Zeda Creed.
-  *
-*/
+// Copyright (c) 2014 Delta Zeda Creed.
 
 package is.deltazedacreed.tictactoe;
 
+/**
+ * Class that runs the game, TicTacToe.
+ */
 public class Game{
-	public static void main(String[] args){
-	}
+    public static void main(String[] args){
+    }
 }
