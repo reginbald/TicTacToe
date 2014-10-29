3d1e0ed3e967d662583ac39ed9f8612f3d94938e
Ingibjorg Osk Jonsdottir <ingibjorgj12@i-bb14f38e.greenqloud.internal>
2014-10-29 21:56:48 +0000

    Refactoring: Moving changePlayers function to Board class.

--- src/main/java/is/deltazedacreed/tictactoe/Game.java
+++ src/main/java/is/deltazedacreed/tictactoe/Game.java
@@ -14,15 +14,6 @@ public class Game{
         board = new Board();
     }
 
-    // Changes player, if player 1 is currently playing, the function switches to player 2
-    public void changePlayer(){
-        if (board.players == 1){
-            board.players = 2;
-        } else {
-            board.players = 1;
-        }
-    }
-
     // Insert a player's character at the correct place.
     public void insert(int x, int y){
         if (board.players == 1){
