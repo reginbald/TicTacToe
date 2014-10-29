22716be1455e47a2448169848ac8fc94536f59ca
asasteinars <asasteinars@gmail.com>
2014-10-29 18:56:28 +0000

    Made function changePlayers()

--- src/main/java/is/deltazedacreed/tictactoe/Game.java
+++ src/main/java/is/deltazedacreed/tictactoe/Game.java
@@ -1,11 +1,23 @@
 // Copyright (c) 2014 Delta Zeda Creed.
 
 package is.deltazedacreed.tictactoe;
+//import package.Board.java;
 
 /**
  * Class that runs the game, TicTacToe.
  */
 public class Game{
+
+     public void changePlayer(Board board){
+        if (board.players == 1){
+            board.players = 2;
+        } else {
+            board.players = 1;
+        }
+    }
+
     public static void main(String[] args){
+        Board board = new Board();
+
     }
 }
