2f0598b7a98f0b2ae7cb247d92c174cf6a7379cb
Ingibjorg Osk Jonsdottir <ingibjorgj12@i-bb14f38e.greenqloud.internal>
2014-10-29 16:10:20 +0000

    Added a test that checks if the game field has correct values.

--- src/test/java/is/deltazedacreed/tictactoe/BoardTest.java
+++ src/test/java/is/deltazedacreed/tictactoe/BoardTest.java
@@ -42,4 +42,18 @@ public class BoardTest{
             }
         }
     }
+
+    @Test
+    // Test if the game field prints out the values of the grid on the correct form
+    public void testGameField(){
+        Board board = new Board();
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 11; j = j + 4){
+                assertEquals('[', board.gameField[i][j]);
+                assertEquals(' ', board.gameField[i][j+1]);
+                assertEquals(']', board.gameField[i][j+2]);
+                assertEquals(' ', board.gameField[i][j+3]);
+            }
+        }
+    }
 }
