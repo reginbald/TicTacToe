4a460d910f9001f36f143b4337a358bc064fc151
asasteinars <asasteinars@gmail.com>
2014-10-29 18:33:58 +0000

    Add test for change player in the game from player1 to player2 and vice versa

--- src/test/java/is/deltazedacreed/tictactoe/GameTest.java
+++ src/test/java/is/deltazedacreed/tictactoe/GameTest.java
@@ -5,9 +5,23 @@ package is.deltazedacreed.tictactoe;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+//import package.Board.java;
 
 /**
  * Class constructor that tests the class Game.
  */
 public class GameTest{
+    //
+    @Test
+    public void testChangePlayer() {
+    	Game test = new Game();
+    	test.players = 1;
+    	test.changePlayer();
+    	assertEquals(2, test.players);
+    	test.changePlayer();
+    	assertEquals(1, test.players); 
+   	}
+
+
+
 }
