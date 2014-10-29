b2ba33a21392081975be903ccd7a58e99ba6df3f
Ingibjorg Osk Jonsdottir <ingibjorgj12@i-bb14f38e.greenqloud.internal>
2014-10-29 19:09:43 +0000

    Added a test that checks if the insert function inserts the correct letter depending on the player.

--- src/test/java/is/deltazedacreed/tictactoe/GameTest.java
+++ src/test/java/is/deltazedacreed/tictactoe/GameTest.java
@@ -20,6 +20,19 @@ public class GameTest{
         assertEquals(2, board.players);
         game.changePlayer(board);
         assertEquals(1, board.players); 
-       }
+    }
 
+    // Test if the insert function inserts the correct letter.
+    @Test
+    public void testInsert(){
+        Game game = new Game();
+        Board board = new Board();
+        game.insert(0, 1);
+
+        if(board.players == 1){
+            assertEquals('o', board.grid[0][1]);
+        } else {
+            assertEquals('x', board.grid[0][1]);
+        }
+    }
 }
