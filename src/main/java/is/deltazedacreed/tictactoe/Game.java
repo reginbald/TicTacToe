06ec5c7d37eaca576d9d513ba4093e81d1e0e6ab
Ingibjorg Osk Jonsdottir <ingibjorgj12@i-bb14f38e.greenqloud.internal>
2014-10-29 23:29:00 +0000

    Added a boolean function that checks if the input is valid

--- src/main/java/is/deltazedacreed/tictactoe/Game.java
+++ src/main/java/is/deltazedacreed/tictactoe/Game.java
@@ -23,6 +23,14 @@ public class Game{
         }
     }
 
+    // Check if input is valid.
+    private boolean isValidInput(int x, int y){
+        if(x >= 0 && x <= 2 && y >= 0 && y <= 2){
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args){
 
     }
