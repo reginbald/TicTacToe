2421240fce4eec9fd4c96ea3ceea6b5aec093f3e
Jon Reginbald Ivarsson <joni12@ru.is>
2014-10-29 15:53:15 +0000

    Setting up spark framework test

---
+++ src/main/java/is/deltazedacreed/tictactoe/TicTacToeWeb.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2014 Delta Zeda Creed.
+ */
+package is.deltazedacreed.tictactoe;
+
+import static spark.Spark.*;
+import spark.*;
+
+/**
+ * Class turns the TicTacToe game in to a web application using the Spark framework. 
+ */
+public class TicTacToeWeb{
+    public static void main(String[] args) {
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello Spark MVC Framework!";
+            }
+        });
+    }
+}
