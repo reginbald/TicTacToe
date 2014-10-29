7ff014f9720e0020de6d21c5d9ddcb625a28f418
Ingibjorg Osk Jonsdottir <ingibjorgj12@i-bb14f38e.greenqloud.internal>
2014-10-29 16:36:50 +0000

    Altered testGameField code. We decided to use string builder to print out the game field.

--- src/test/java/is/deltazedacreed/tictactoe/BoardTest.java
+++ src/test/java/is/deltazedacreed/tictactoe/BoardTest.java
@@ -48,12 +48,12 @@ public class BoardTest{
     public void testGameField(){
         Board board = new Board();
         for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 11; j = j + 4){
-                assertEquals('[', board.gameField[i][j]);
-                assertEquals(' ', board.gameField[i][j+1]);
-                assertEquals(']', board.gameField[i][j+2]);
-                assertEquals(' ', board.gameField[i][j+3]);
+            for (int j = 0; j < 3; ++j){
+                assertEquals('[', board.gameField.charAt(j));
+                assertEquals(' ', board.gameField.charAt(++j));
+                assertEquals(']', board.gameField.charAt(++j));
             }
+            assertEquals('/n', board.gameField.charAt(9));
         }
     }
 }
