294037ec6928e033fc58684dcdfa407894bf9f09
Ingibjorg Osk Jonsdottir <ingibjorgj12@i-bb14f38e.greenqloud.internal>
2014-10-30 09:43:50 +0000

    Added a test that checks a winning column

--- src/test/java/is/deltazedacreed/tictactoe/GameTest.java
+++ src/test/java/is/deltazedacreed/tictactoe/GameTest.java
@@ -66,13 +66,22 @@ public class GameTest{
     }
 
     @Test
-    // Check the WinRow function
+    // Check the winRow function
     public void testWinRow(){
         Game game = new Game();
-
         game.insert(0, 0);
         game.insert(0, 1);
         game.insert(0, 2);
         assertEquals(true, game.winRow());
+    }
+
+    @Test
+    // Check the winColun function
+    public void testWinColumn(){
+        Game game = new Game();
+        game.insert(0, 0);
+        game.insert(1, 0);
+        game.insert(2, 0);
+        assertEquals(true, game.winColumn());
     }
 }
